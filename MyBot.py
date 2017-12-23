aecdf90a37f5c6d12a339d74ad36cb5e7fbf6e96
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2017-12-23 19:54:39 +0100

    Attack nearest target regardless of already enganged ships

--- MyBot.py
+++ MyBot.py
@@ -7,7 +7,6 @@ def perform_turn():
     game_map = game.update_map()
     command_queue = []
     planet_conquer_queue = []
-    ship_attack_queue = []
     my_ships = game_map.get_me().all_ships()
     for ship in my_ships:
         if ship.docking_status != ship.DockingStatus.UNDOCKED:
@@ -20,8 +19,8 @@ def perform_turn():
                     planet for planet in planets_by_distance if planet.owner != game_map.get_me()
                 ]
                 if len(enemy_planets_by_distance) > 0:
-                    enqueue_attack_of_docked_ships(command_queue, game_map, planet, ship_attack_queue, ship)
-                    break
+                    enqueue_attack_of_docked_ships(command_queue, game_map, enemy_planets_by_distance[0], ship)
+                break
 
             if planet.is_owned() or planet in planet_conquer_queue:
                 continue
@@ -56,19 +55,17 @@ def all_planets_are_owned(game_map):
     return True
 
 
-def enqueue_attack_of_docked_ships(command_queue, game_map, planet, ship_attack_queue, ship):
+def enqueue_attack_of_docked_ships(command_queue, game_map, planet, ship):
     all_docked_enemy_ships = [ship for ship in planet.all_docked_ships() if ship.owner != game_map.get_me()]
     for enemy_ship in all_docked_enemy_ships:
-        if enemy_ship not in ship_attack_queue:
-            navigate_command = ship.navigate(
-                ship.closest_point_to(enemy_ship, 0),
-                game_map,
-                speed=int(hlt.constants.MAX_SPEED)
-            )
-            ship_attack_queue.append(enemy_ship)
-            if navigate_command:
-                command_queue.append(navigate_command)
-            break
+        navigate_command = ship.navigate(
+            ship.closest_point_to(enemy_ship, 0),
+            game_map,
+            speed=int(hlt.constants.MAX_SPEED)
+        )
+        if navigate_command:
+            command_queue.append(navigate_command)
+        break
 
 
 if __name__ == "__main__":
